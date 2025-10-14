19edc06f0a088ca6d81e580395317c955ed7610e
Marlon <Marlonschlosshauer@gmail.com>
2025-10-14 16:14:21 +0200

    Fix hard coded parameters in GTM provider

--- packages/next/tracking/gtm/index.tsx
+++ packages/next/tracking/gtm/index.tsx
@@ -16,6 +16,8 @@ export interface GtmProviderProps {
     hasConsent: boolean;
     onBeforeGtmSetup?: () => void;
     onAfterGtmSetup?: () => void;
+    gtmId: string;
+    gtmDomain: string;
 }
 
 export interface GtmProviderData {
@@ -28,6 +30,8 @@ export const GtmProvider: FC<PropsWithChildren<GtmProviderProps>> = ({
     hasConsent,
     onBeforeGtmSetup,
     onAfterGtmSetup,
+    gtmId,
+    gtmDomain,
     children,
 }) => {
     useDataEventForwarder(hasConsent);
@@ -55,8 +59,8 @@ export const GtmProvider: FC<PropsWithChildren<GtmProviderProps>> = ({
                       (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
                       new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
                       j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-                      'https://dt.visit-bw.com/dt.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-                      })(window,document,'script','dataLayer','${process.env.NEXT_PUBLIC_GTM_ID}');
+                      'https://${gtmDomain}/dt.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                      })(window,document,'script','dataLayer','${gtmId}');
                     `}
                 </Script>
             )}
